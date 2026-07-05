26a5c72041e93bc9166e8dc53aa5939fba48e7a9
Sparsh Gahoi <sparsh@Sparshs-MacBook-Air.local>
2026-07-06 00:13:53 +0530

    Update CORS for deployment

--- app/main.py
+++ app/main.py
@@ -22,15 +22,10 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan
 )
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
