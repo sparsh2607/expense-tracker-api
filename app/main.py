5e1e7894de3e61d6e02ee92b56cd207f1ac1749b
Sparsh Gahoi <sparsh@Sparshs-MacBook-Air.local>
2026-07-06 00:52:37 +0530

    middleware changes

--- app/main.py
+++ app/main.py
@@ -24,15 +24,11 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://spendwise-expense-tracker-19b29tctp-sparsh2607s-projects.vercel.app/"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 def home():
